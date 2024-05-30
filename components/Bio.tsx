fbde38a8d0c0d2266256ebb0dc596c946fbf6267
devfxx <fxx@kty.lol>
2024-05-31 01:21:07 +0300

    refactor: Update Bio component with responsive font sizes

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -4,14 +4,16 @@ import React from "react";
 const Bio: React.FC = () => {
   return (
     <div className="text-white p-20">
-      <h1 className="text-6xl font-bold mb-2 font-khula">
+      <h1 className="text-4xl md:text-6xl font-bold mb-2 font-khula">
         <span className="bg-gradient-to-r from-white to-cum-purple text-transparent bg-clip-text">
           Fxx
         </span>
       </h1>
       <div className={mono.className}>
-        <h2 className="text-xl mt-4 font-mono">Software / DevOps Wizard</h2>
-        <p className="text-lg mt-4 font-mono">
+        <h2 className="text-lg md:text-xl mt-4 font-mono">
+          Software / DevOps Wizard
+        </h2>
+        <p className="text-base md:text-lg mt-4 font-mono">
           5 Years of Experience
           <br />
           Golang and TypeScript
