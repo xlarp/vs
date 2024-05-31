565ee0eac04bfc0f75458123c19a20547aef6a96
devfxx <fxx@kty.lol>
2024-05-31 03:07:17 +0300

    refactor: Update Footer component with improved spacing and layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="text-white bg-cust-gray py-12 px-4 md:px-0">
+    <footer className="text-white bg-cust-gray py-12 px-8 md:px-0">
       <div className="container mx-auto flex flex-col md:flex-row justify-between">
         <div className="text-2xl mb-4 font-mono">
           <div className={mono.className}>
@@ -18,7 +18,7 @@ const Footer: React.FC = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-16">
+        <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-16">
           <div>
             <div className="font-medium mb-4 flex items-center space-x-2 transition-all duration-500 ease-in-out transform hover:scale-105 text-white hover:text-blue-400">
               <WorldIcon className="h-5 w-5" />
